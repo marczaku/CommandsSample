1106a55ad8d32f6d829a4fe09212eaa9340a081b
Marc Zaku <mzaku@hotmail.de>
2021-05-04 17:22:35 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CommandsSample
+ 
