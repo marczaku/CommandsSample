a248f25914d9cb2b0ee21a36ef5c7d1688bd4be8
Marc Zaku <mzaku@hotmail.de>
2021-05-04 18:04:29 +0200

    Uses reflection to find all commands

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace CommandsSample
+﻿using System;
+using System.Reflection;
+
+namespace CommandsSample
 {
     class Program
     {
         static void Main(string[] args) {
-            new Server().Run(args);
-            new Client().Run(args);
+            var assembly = Assembly.GetExecutingAssembly();
+            foreach (var type in assembly.GetTypes()) {
+                if (!type.IsAssignableTo(typeof(Command))) {
+                    Console.WriteLine($"{type.Name} does not inherit from Command.");
+                    continue;
+                }
+
+                if (type.IsAbstract) {
+                    Console.WriteLine($"{type.Name} is Abstract.");
+                    continue;
+                }
+                Console.WriteLine($"Found Command: {type.Name}");
+                var constructor = type.GetConstructor(new Type[0]);
+                var instance = constructor.Invoke(new object[0]);
+                var command = instance as Command;
+                command.Run(args);
+            }
         }
     }
 }
