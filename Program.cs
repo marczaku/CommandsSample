2d55c4f0fb094b5675e5df9eaba6a8bf2babac5f
Marc Zaku <mzaku@hotmail.de>
2021-05-04 17:35:12 +0200

    simple sample for two applications in one.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace CommandsSample
 {
@@ -6,7 +9,22 @@ namespace CommandsSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(args.Length > 0) { // server code:
+                var listener = new TcpListener(IPAddress.Any, 4444);
+                listener.Start();
+                var client = listener.AcceptTcpClient();
+                var stream = client.GetStream();
+                using var writer = new StreamWriter(stream);
+                writer.Write("Hello World!");
+            }
+            else
+            { // client code: 
+                var client = new TcpClient(new IPEndPoint(IPAddress.Any, 4445));
+                client.Connect(IPAddress.Loopback, 4444);
+                var stream = client.GetStream();
+                var response = new StreamReader(stream).ReadToEnd();
+                Console.WriteLine(response);
+            }
         }
     }
 }
